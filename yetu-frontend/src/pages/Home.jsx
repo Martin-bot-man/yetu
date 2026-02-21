945029cac3d9727086e7db60889b81eccd1d7df0
martin <martinwere14@gmail.com>
2026-02-21 21:07:27 +0300

    feat: built the header and footer components

---
+++ yetu-frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BottomNav from '../components/shared/BottomNav'
+
+const Home = () => {
+  return (
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex'>
+        {/* {left div} */}
+        <div className="flex-3 bg-[#1a1a1a]"></div>
+        {/* {right div} */}
+        <div className="flex-2 bg-[#1a1a1a]"></div>
+      <BottomNav />
+    </section>
+  )
+}
+
+export default Home
