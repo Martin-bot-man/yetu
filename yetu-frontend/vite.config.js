5b6ab740b6b68b585d5bc1f893e36dd6ef38a1cf
martin <martinwere14@gmail.com>
2026-02-22 10:44:41 +0300

    feat: created popular dishes page

--- yetu-frontend/vite.config.js
+++ yetu-frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-    require('tailwind-scrollbar-hide'),
+    import('tailwind-scrollbar-hide'),
 
   ],
 })
