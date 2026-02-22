c96f0002a261bab006c5b01b3a112476cb14280b
martin <martinwere14@gmail.com>
2026-02-22 08:36:00 +0300

    feat added order list section

--- yetu-frontend/src/components/shared/Header.jsx
+++ yetu-frontend/src/components/shared/Header.jsx
@@ -16,6 +16,7 @@ const Header = () => {
       {/* {/SEARCH} */}
       <div className='flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-5 py-2 
       w-125'>
+        <FaSearch className='text-[#f5f5f5]'/>
         <input type="text"
         placeholder='Search'
         className="bg-[#1f1f1f] outline-none  text-[#f5f5f5]" />
